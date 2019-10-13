3b9c3c22923bf8eb444a1efe953dc85c95283956
Alexei Rostovshchikov <alexei.rosto@gmail.com>
2019-10-13 22:38:38 +0300

    init

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+    api.cache(true);
+
+    const presets = [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 'current',
+                },
+            },
+        ],
+    ];
+    const plugins = [];
+
+    return {
+        presets,
+        plugins
+    };
+}
